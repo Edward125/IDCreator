548d95efe23c51fb1416cde0d1ebb120761f95b9
Edward <edward_song@yeah.net>
2017-03-29 21:25:34 +0800

    1st commit

---
+++ IDCreator/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace IDCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOld15 = new System.Windows.Forms.TextBox();
+            this.txtNew18 = new System.Windows.Forms.TextBox();
+            this.txtNew17 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtSex = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtOld15);
+            this.groupBox1.Controls.Add(this.txtNew18);
+            this.groupBox1.Controls.Add(this.txtNew17);
+            this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(419, 136);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "15位號碼升級以及新號碼校驗";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "18位身份證:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "身份證號前17位:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "15位身份證:";
+            // 
+            // txtOld15
+            // 
+            this.txtOld15.Location = new System.Drawing.Point(138, 36);
+            this.txtOld15.MaxLength = 15;
+            this.txtOld15.Name = "txtOld15";
+            this.txtOld15.Size = new System.Drawing.Size(233, 26);
+            this.txtOld15.TabIndex = 2;
+            this.txtOld15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtOld15.TextChanged += new System.EventHandler(this.txtOld15_TextChanged);
+            this.txtOld15.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtOld15_KeyPress);
+            // 
+            // txtNew18
+            // 
+            this.txtNew18.Location = new System.Drawing.Point(138, 93);
+            this.txtNew18.Name = "txtNew18";
+            this.txtNew18.ReadOnly = true;
+            this.txtNew18.Size = new System.Drawing.Size(233, 26);
+            this.txtNew18.TabIndex = 1;
+            this.txtNew18.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNew17
+            // 
+            this.txtNew17.Location = new System.Drawing.Point(138, 64);
+            this.txtNew17.MaxLength = 17;
+            this.txtNew17.Name = "txtNew17";
+            this.txtNew17.Size = new System.Drawing.Size(233, 26);
+            this.txtNew17.TabIndex = 0;
+            this.txtNew17.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNew17.TextChanged += new System.EventHandler(this.txtNew17_TextChanged);
+            this.txtNew17.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNew17_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.btnCreate);
+            this.groupBox2.Controls.Add(this.txtDate);
+            this.groupBox2.Controls.Add(this.txtSex);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.txtAddress);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtID);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 154);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(419, 136);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "隨機生成身份證號碼";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(314, 99);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 30);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "保存號碼";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(314, 61);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(99, 32);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "生成號碼";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(301, 29);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.ReadOnly = true;
+            this.txtDate.Size = new System.Drawing.Size(112, 26);
+            this.txtDate.TabIndex = 12;
+            this.txtDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSex
+            // 
+            this.txtSex.Location = new System.Drawing.Point(176, 29);
+            this.txtSex.Name = "txtSex";
+            this.txtSex.ReadOnly = true;
+            this.txtSex.Size = new System.Drawing.Size(52, 26);
+            this.txtSex.TabIndex = 11;
+            this.txtSex.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(56, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(75, 26);
+            this.txtName.TabIndex = 10;
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(75, 61);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.ReadOnly = true;
+            this.txtAddress.Size = new System.Drawing.Size(233, 26);
+            this.txtAddress.TabIndex = 9;
+            this.txtAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "家庭地址:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(232, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "出生日期:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(134, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "性別:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "姓名:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "身份證:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(75, 93);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(233, 26);
+            this.txtID.TabIndex = 1;
+            this.txtID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 295);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNew18;
+        private System.Windows.Forms.TextBox txtNew17;
+        private System.Windows.Forms.TextBox txtOld15;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtSex;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ IDCreator/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IDCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 參數
+
+
+        int[] weight = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
+        char[] validdate = { '1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+        string id = string.Empty;
+        #endregion
+
+
+        private char  getValidateCode(string id17)
+        {
+
+            int sum = 0;
+            int mode = 0;           
+
+            for (int i = 0; i < id17.Length; i++)
+            {
+                sum = sum + Convert.ToInt16(id17.Substring(i, 1)) * weight[i];
+            }
+            mode = sum % 11;
+            return validdate[mode];
+
+        }
+
+        private void txtNew17_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNew17.Text.Trim().Length == 17)
+            {
+                txtNew18.Text = txtNew17.Text.Trim() + getValidateCode(txtNew17.Text.Trim());
+            }
+        }
+
+        private void txtNew17_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 & e.KeyChar <= 57 | e.KeyChar == 8)
+            {
+               // e.KeyChar = Convert.ToChar(8);
+            }
+            else
+            {
+                e.KeyChar = Convert.ToChar(0);
+            }
+        }
+
+        private void txtOld15_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 & e.KeyChar <= 57 | e.KeyChar == 8)
+            {
+                // e.KeyChar = Convert.ToChar(8);
+            }
+            else
+            {
+                e.KeyChar = Convert.ToChar(0);
+            }
+        }
+
+        private void txtOld15_TextChanged(object sender, EventArgs e)
+        {
+            if (txtOld15.Text.Length <= 6)
+            {
+                txtNew17.Text = txtOld15.Text;
+            }
+            else
+            {
+                txtNew17.Text = txtOld15.Text.Substring(0, 6) + "19" + txtOld15.Text.Substring(6, txtOld15.Text.Length - 6);
+            }            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "身份證號生成驗證器,Ver:" + Application.ProductVersion;
+        }
+
+
+
+
+
+        #region Person
+
+
+
+        enum sex
+        {
+            男,
+            女
+        }
+
+
+        struct Person
+        {
+
+            string Name;
+            sex Sex;
+            string Birthday;
+            string Address;
+
+        }  
+
+
+        #endregion
+
+
+        #region create ranom nunmber
+
+        /// <summary>
+        /// create a random number between min ~ max
+        /// </summary>
+        /// <param name="min">min</param>
+        /// <param name="max">max</param>
+        /// <returns></returns>
+        public int CreateRandNum(int min, int max)
+        {
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            return r.Next(min, max);
+        }
+
+        #endregion
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            txtName.Text = GenCnName.getRandomName();
+            string ID = GenPinCode.GetGenPinCode();
+            txtID.Text = ID.Substring(0, 18);
+            txtAddress.Text = ID.Remove(0, 18);
+            //MessageBox.Show(txtID.Text.Substring(16, 1));
+            int i = Convert.ToInt16(txtID.Text.Substring(16, 1));
+            if (i % 2 == 0)
+                txtSex.Text = "女";
+            else
+                txtSex.Text = "男";
+
+            txtDate.Text = txtID.Text.Substring(6, 4) + "年" + txtID.Text.Substring(10, 2) + "月" + txtID.Text.Substring(12, 2) + "日";
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+            if (!string.IsNullOrEmpty(txtID.Text.Trim()) && id != txtID .Text )
+            {
+                StreamWriter sw = new StreamWriter(Application.StartupPath + @"\ID_INFO_LIST.txt", true);
+                string head = "--------------------" + DateTime.Now.ToString("yyyyMMddHHmmss") + "--------------------";
+                sw.WriteLine(head);
+                sw.WriteLine("Name:" + txtName.Text);
+                sw.WriteLine("Sex:" + txtSex.Text);
+                sw.WriteLine("Birthday:" + txtDate.Text);
+                sw.WriteLine("Address:" + txtAddress.Text);
+                sw.WriteLine("ID:" + txtID.Text);
+                sw.Close();
+                id = txtID.Text;
+            }
+            
+        }
+    }
+}
